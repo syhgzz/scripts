c99e06648373ef0614cd4436628c98711f2be6b4
Zhou Zhuo <syhgzz@163.com>
2024-08-21 17:14:30 +0800

    GBK

--- rmduplicatefiles.ps1
+++ rmduplicatefiles.ps1
@@ -1,6 +1,12 @@
-# 鍘绘帀閲嶅鏂囦欢 by zhouzhuo
+# 去掉重复文件 by zhouzhuo
+
+
 $map = New-Object 'System.Collections.Generic.Dictionary[string,string]'
 
 $map.Add("WG", "file1.jpg")
 $map.Add("WG2", "file2.jpg")
 
+
+$workdir = "C:\Users\zhouzhuo\Lab\Repo\新建文件夹\"
+
+Get-Item -Path $workdir
