649f62b146ff85ddfa8e3250ae26cab3d098cb7d
Zhou Zhuo <syhgzz@163.com>
2024-08-23 14:38:33 +0800

    增加PowerShell7.4支持的UTF-8脚本文件

---
+++ rmDF_UTF8.ps1
@@ -0,0 +1,84 @@
+# 去掉重复文件 by zhouzhuo 2024.8.22
+#PowerShell7.4支持的UTF-8脚本文件, 但 Windows PowerShell 5.1 仅支持GBK编码的脚本文件
+
+#-------------------------控制参数-------------------------
+
+#工作目录, 可以修改. 注意结尾要有逗号,
+$dirs = @(
+"C:\Users\zhouzhuo\Lab\Repo\新建文件夹",
+"C:\Users\zhouzhuo\Lab\Repo\新建文件夹2",
+"C:\Users\zhouzhuo\Lab\Repo\新建文件夹3",
+"")
+
+#是否到子文件夹中执行去重操作, 如果不需要就改为0.
+$recurse = 1 
+
+#重复项目放到此目录, 可改名
+$duplicateName = "重复项目"
+
+#-------------------------控制参数-------------------------
+
+
+#-------------------------程序代码-------------------------
+
+function Remove-DuplicatedFile 
+{
+    
+    param (
+        $workDir
+    )
+    if($workDir -eq "") {return}
+    # $workDir = "C:\Users\zhouzhuo\Lab\Repo\新建文件夹"
+    #重复项目放到此目录
+    $duplicateDir = "$workDir\$duplicateName"
+    [bool]$hasduplicatedir = Test-Path -Path $duplicateDir #测试是否已经存在
+
+    #获取工作目录的对象
+    $dirarray = @()
+    $dirarray += (Get-Item -Path $workDir)
+
+    #递归获取子文件夹对象, 并排除"重复项目"目录
+    if($recurse -eq $true)
+    {
+        $subdirarray = Get-ChildItem -Path $workDir -Recurse | Where-Object {$_.Attributes -eq "Directory"} | Where-Object{$_.FullName -ne $duplicateDir}
+        $dirarray += $subdirarray
+    }
+
+    #获取所有文件项目, 排除文件夹项目
+    $filearray = Get-ChildItem  $dirarray.FullName | Where-Object {$_.Attributes -ne "Directory"}
+
+    #每次要新建立一个map对象
+    $map = New-Object 'System.Collections.Generic.Dictionary[string,string]' 
+
+    foreach($item in $filearray)
+    {
+        $file= $item.FullName
+        $md5 = Get-FileHash -Path $file -Algorithm MD5
+        $key = $md5.Hash
+        $value = $md5.Path
+        if($map.ContainsKey($key) -eq $true)
+        {
+            if($hasduplicatedir -eq $false)
+            {
+                New-Item -ItemType "directory" -Path $duplicateDir | Out-Null
+                $hasduplicatedir = $true
+            }
+            Move-Item  -Path $value -Destination $duplicateDir -Force
+            echo "重复项 $value 移到 $duplicateDir"
+        }
+        else 
+        {
+            $map.Add($key, $value)
+        }
+    }
+    $map.Clear() #清理, 放置内存溢出
+
+
+}
+
+foreach($dir in $dirs)
+{
+    if($dir -eq "") {break}
+    echo "正处理目录: $dir"
+    Remove-DuplicatedFile -workDir $dir
+}
