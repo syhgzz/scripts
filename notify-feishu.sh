2c99709320d02a5f8055bb9e906c779c1d0bb13a
zhouzhuo <syhgzz@163.com>
2026-09-26 11:48:24 +0800

    修复过滤词的bug

--- notify-feishu.sh
+++ notify-feishu.sh
@@ -4,5 +4,5 @@ title=$(echo "$payload" | jq -r '.session_title // "Kimi Code 会话"')
 cwd=$(echo "$payload" | jq -r '.cwd // ""')
 
 curl -s -X POST -H "Content-Type: application/json" \
-  -d "{\"msg_type\":\"text\",\"content\":{\"text\":\"[notify] Kimi Code 任务结束\n会话：${title}\n目录：${cwd}\n时间：$(date '+%F %T')\"}}" \
+  -d "{\"msg_type\":\"text\",\"content\":{\"text\":\"Kimi Code 任务结束\n会话：${title}\n目录：${cwd}\n时间：$(date '+%F %T')\"}}" \
 https://open.feishu.cn/open-apis/bot/v2/hook/74efe7c2-82d8-481f-a092-9aa1d85849a0
